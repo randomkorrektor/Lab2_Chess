e7d7c41bf740d19ec6f7c202b2873182266e4002
RanKor <randomkorrektor@gmail.com>
2016-10-23 23:01:00 +0700

    Create adding players

---
+++ Chess/Managers/Manager.cs
@@ -0,0 +1,39 @@
+﻿using DataTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Managers
+{
+    public class Manager
+    {
+        public static Manager Instance = new Manager();
+        private Manager() { }
+
+        public List<Player> Players = new List<Player>();
+        CardDeck CardDeck = new CardDeck();
+        Table Table;
+
+        public static void GameStart(object state)
+        {
+
+        }
+
+        TimerCallback timeCB = new TimerCallback(GameStart);
+        public List<Player> AddPlayer(string name)
+        {
+            Players.Add(new Player(name, 10000));
+            if(Players.Count==1)
+            {
+                Timer timer = new Timer(GameStart, null, 120000, Timeout.Infinite);
+            }
+            return Players;
+        }
+
+        
+        
+    }
+}
