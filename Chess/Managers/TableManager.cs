d169ba2bbfc6b35ffe7283ff85bf72193acf77cc
RanKor <randomkorrektor@gmail.com>
2016-10-23 21:48:45 +0700

    Start create Managers

---
+++ Chess/Managers/TableManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Managers
+{
+    public class TableManager
+    {
+        public static TableManager Instance = new TableManager();
+        private TableManager() { }
+
+
+    }
+}
---
+++ Chess/Managers/UserManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Managers
+{
+    public class UserManager
+    {
+        public static UserManager Instance = new UserManager();
+        private UserManager() { }
+
+
+    }
+}
