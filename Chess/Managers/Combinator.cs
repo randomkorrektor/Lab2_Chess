42ab16bafdb1144aa256ae1608d1f14c7557fff7
RanKor <randomkorrektor@gmail.com>
2016-11-12 21:55:18 +0700

    Start develope on JS

---
+++ Chess/Managers/Combinator.cs
@@ -0,0 +1,22 @@
+﻿using DataTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Managers
+{
+    public class Combinator
+    {
+
+        public int IsFRoyalFlush(List<Card> cards)
+        {
+            if(cards.Last().Rating == Rating.Ace && (int)(cards[2].Rating)>9)
+            {
+
+            }
+            return -1;
+        }
+    }
+}
