26a1e15361a1d13a8d6a79cc053513d881515f40
lochid <pmi22dremov@gmail.com>
2016-11-13 01:41:09 +0700

    Create Card class.

---
+++ Chess/NodeChessServer/DataTypes/Card.js
@@ -0,0 +1,13 @@
+﻿class Card {
+    constructor(lear, rating) {
+        this.lear = lear;
+        this.rating = rating;
+        this.open = false;
+    }
+
+    clone() {
+        return new Card(this.lear, this.rating);
+    }
+}
+
+module.exports = Card;
--- Chess/NodeChessServer/app.js
+++ Chess/NodeChessServer/app.js
@@ -2,8 +2,11 @@
 const Bank = require('./DataTypes/Bank');
 const Lear = require('./DataTypes/Lear');
 const Rating = require('./DataTypes/Rating');
+const Card = require('./DataTypes/Card');
 
 const bank = new Bank(1);
 console.log(bank);
 console.log(Lear);
 console.log(Rating);
+const card = new Card(Lear.Diamonds, Rating.Ten);
+console.log(card);
