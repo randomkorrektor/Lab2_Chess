b384263ad6ad9d8665a0bd79700d085f374f2184
lochid <pmi22dremov@gmail.com>
2016-11-13 14:29:36 +0700

    Create io manager.

---
+++ Chess/NodeChessServer/Managers/IOManager.js
@@ -0,0 +1,66 @@
+﻿class IOManager {
+    constructor() {
+        this.timer = null;
+        this.TimerTick = null;
+        this.TimerDisposed = null;
+
+        this.StartGame = null;
+        this.StartRound = null;
+        this.RefreshTable = null;
+        this.PlayerWin = null;
+        this.userManager = null;
+    }
+
+    StartTimer() {
+        this.timer.Start();
+    }
+
+    DisposeTimer() {
+        timer.Dispose();
+    }
+
+    Timer_Elapsed() {
+        this.TimerTick(timer.date);
+        if (timer.date >= 1200000) {
+            timer.Dispose();
+        }
+    }
+
+    Timer_Disposed() {
+        TimerDisposed();
+    }
+
+    AddUser(name, refresh) {
+        return this.userManager.AddUser(name, refresh);
+    }
+
+    RateUser(player, com, rise) {
+        GameManager.Instance.RateOfPayer(player, com, rise);
+    }
+
+    StartingGame() {
+        StartGame();
+    }
+
+    StartingRound() {
+        StartRound();
+    }
+
+    RefreshingTable(table) {
+        RefreshTable(table);
+    }
+
+    RefreshingPlayers() {
+        var users = this.UserManager.Users;
+
+        for (let user of users) {
+            user.Refresh();
+        }
+    }
+
+    PlayerWin(player) {
+        PlayerIsWin(player);
+    }
+}
+
+module.exports = IOManager;
