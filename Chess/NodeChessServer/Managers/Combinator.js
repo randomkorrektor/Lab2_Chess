7879e7d55c0912417755c27a9b4f11d62c53b844
RanKor <randomkorrektor@gmail.com>
2016-11-13 14:44:40 +0700

    Create Combinator 9&10

---
+++ Chess/NodeChessServer/Managers/Combinator.js
@@ -0,0 +1,30 @@
+﻿class combinator {
+
+    IsRoyalFlush(cards) {
+        let lears = [0, 0, 0, 0];
+        for (const card of cards) {
+            if (card.rating > 9) {
+                lears[card.lear]++;
+            }
+        }
+        if (lears.find(u=> (u > 4)))
+            return 10;
+        return -1;
+    }
+
+    IsStritFlush(cards) {
+        let lears = [{ rating: null, count: 0 }, { rating: null, count: 0 }, { rating: null, count: 0 }, { rating: null, count: 0 }];
+        for (const card of cards) {
+            if (lears[card.lear].rating == null ||
+                lears[card.lear].rating - 1 == card.rating) {
+                lears[card.lear].rating = card.rating;
+                lears[card.lear].count++;
+            }
+        }
+        if (lears.find(u => (u.count > 4)))
+            return 9
+        return -1;
+    }
+    
+
+}
